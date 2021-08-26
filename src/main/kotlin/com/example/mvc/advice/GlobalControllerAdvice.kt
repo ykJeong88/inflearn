7b69b757167e63093ed881320e9d1d64836acf5c
JeongYoungKyun <mohargga@gmail.com>
2021-08-26 23:46:23 +0900

    ExceptionHandler를 활용하여 자세한 에러 정보 내려주기 (1)

--- src/main/kotlin/com/example/mvc/advice/GlobalControllerAdvice.kt
+++ src/main/kotlin/com/example/mvc/advice/GlobalControllerAdvice.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 
-@RestControllerAdvice(basePackageClasses = [ExceptionApiController::class])
+//@RestControllerAdvice(basePackageClasses = [ExceptionApiController::class])
 class GlobalControllerAdvice {
 
     @ExceptionHandler(value = [RuntimeException::class])
--- src/main/kotlin/com/example/mvc/controller/exception/ExceptionApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/exception/ExceptionApiController.kt
@@ -1,15 +1,22 @@
 package com.example.mvc.controller.exception
 
+import com.example.mvc.model.http.Error
+import com.example.mvc.model.http.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+import javax.servlet.http.HttpServletRequest
+import javax.validation.ConstraintViolationException
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 
 @RestController
 @RequestMapping("/api/exception")
+@Validated
 class ExceptionApiController {
 
     @GetMapping("/hello")
@@ -20,6 +27,50 @@ class ExceptionApiController {
 
         val list = mutableListOf<String>()
         val temp = list[0]
+    }
+
+    @GetMapping("")
+    fun get(
+
+        @NotBlank
+        @Size(min=2, max=6)
+        @RequestParam name:String,
+
+        @Min(10)
+        @RequestParam age: Int
+    ): String {
+        println(name)
+        println(age)
+
+        return name + " "  + age
+    }
+
+    @ExceptionHandler(value= [ConstraintViolationException::class])
+    fun constraintViolationExceptin(e:ConstraintViolationException, request:HttpServletRequest) : ResponseEntity<ErrorResponse>{
+        // 1.에러 분석
+        val errors = mutableListOf<Error>()
+
+        e.constraintViolations.forEach {
+            val field = it.propertyPath.last().name
+            val message = it.message
+            val error = Error().apply {
+                this.field = field
+                this.message = message
+            }
+            errors.add(error)
+        }
+        // 2. ErrorResponse
+        val errorResponse = ErrorResponse().apply{
+            this.resultCode="fail"
+            this.httpStatus=HttpStatus.BAD_REQUEST.value().toString()
+            this.message="요청에 에러가 발생했습니다."
+            this.path=request.requestURI.toString()
+            this.timestamp= LocalDateTime.now()
+            this.errors=errors
+        }
+
+        // 3. ResponseEntity
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
     }
 
     @ExceptionHandler(value = [IndexOutOfBoundsException::class])
